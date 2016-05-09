a89f5a9837f8ee0fbb63fa7e58b9e02d24877d78
Amir Fayek <afayek88@gmail.com>
2016-05-09 16:46:59 -0500

    change db to pg from sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160430212448) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "ages", force: :cascade do |t|
     t.integer  "organization_id"
     t.integer  "minimum_age"
