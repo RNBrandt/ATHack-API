99c636f1197edd29de9d1d8967e38ddc638c3911
Amir Fayek <afayek88@gmail.com>
2016-05-09 16:36:26 -0500

    remove uneeded param

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -24,7 +24,6 @@ class OrganizationsController < ActionController::API
         :age,
         :language,
         :service_hour,
-        :fee,
-        {languages: :language})
+        :fee)
   end
 end
