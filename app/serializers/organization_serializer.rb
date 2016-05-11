4d78ee3274cbe71f81a685b29d3d0ed1db3159bf
Reuben Brandt <Reubennbrandt@gmail.com>
2016-05-10 18:47:00 -0700

    Add phone number serializer with gem

--- app/serializers/organization_serializer.rb
+++ app/serializers/organization_serializer.rb
@@ -1,5 +1,5 @@
 class OrganizationSerializer < ActiveModel::Serializer
-  attributes :org_type, :name, :address, :website, :email, :description, :fee, :languages, :genders, :provided_services
+  attributes :org_type, :name, :address, :website, :email, :description, :fee, :languages, :genders, :provided_services, :phone_numbers
   has_one  :age, class_name: "Age"
   delegate :cache_key, to: :object
 
@@ -13,5 +13,9 @@ class OrganizationSerializer < ActiveModel::Serializer
 
   def provided_services
     object.provided_services.pluck(:service_type)
+  end
+
+  def phone_numbers
+    object.phone_numbers.pluck(:phone_numbers)
   end
 end
--- app/serializers/phone_number_serializer.rb
+++ app/serializers/phone_number_serializer.rb
@@ -1,4 +1,2 @@
 class PhoneNumberSerializer < ActiveModel::Serializer
-  attributes :description, :number
-  belongs_to :organization
 end
