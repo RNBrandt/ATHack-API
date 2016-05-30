a4e8ac6e6e23255c5f4e7abee11d458e9d75e12e
Reuben Brandt <Reubennbrandt@gmail.com>
2016-05-29 18:01:41 -0700

    Add code to get Org from multiple values, the time and space recs are too much and should be refactored

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -4,14 +4,20 @@ class OrganizationsController < ActionController::API
             @organizations = Organization.all
         else
             # as this is, the params must match exactly, I'm going to work on code to make each one individually searchable
-            @organzations = Organization.where(organization_params)
+            # @organizations = []
+             @organizations = Organization.where(organization_params)
+            # return_value.each do |org|
+            #     @organizations << organization_params
+                # if org."#{organization_params}".includes(:organization_params)
+                #     @organzations << org
+                # end
+            # end
             # if :organization_params.kind_of?(Array)
             #     p "do something here"
             # end
         end
-        render json: @organzations
+        render json: @organizations
     end
-
     private
 
     def organization_params
@@ -30,3 +36,24 @@ class OrganizationsController < ActionController::API
         :fee)
   end
 end
+
+
+
+# this returns an array with all the phone_number objects from the requested Organizations
+def get_phone_numbers(phone_number)
+    orgs = Organization.all
+    organizations_phone_number_objects = []
+    orgs.each do |org|
+        organizations_phone_number_objects << org.phone_numbers
+    end
+    organizations_phone_number_objects.each do |phone|
+        phone.each do |number_object|
+            if number_object.number == phone_number
+                return number_object.organization_id
+            end
+        end
+    end
+end
+
+get_phone_numbers("(510) 251-2292")
+
