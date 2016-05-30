296bc0bb65b614891045c123c452659d492b36fb
Reuben Brandt <Reubennbrandt@gmail.com>
2016-05-30 15:14:50 -0700

    Initial join table solution to attribute search problem

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -3,18 +3,7 @@ class OrganizationsController < ActionController::API
         if params.empty?
             @organizations = Organization.all
         else
-            # as this is, the params must match exactly, I'm going to work on code to make each one individually searchable
-            # @organizations = []
              @organizations = Organization.where(organization_params)
-            # return_value.each do |org|
-            #     @organizations << organization_params
-                # if org."#{organization_params}".includes(:organization_params)
-                #     @organzations << org
-                # end
-            # end
-            # if :organization_params.kind_of?(Array)
-            #     p "do something here"
-            # end
         end
         render json: @organizations
     end
@@ -37,23 +26,10 @@ class OrganizationsController < ActionController::API
   end
 end
 
-
-
-# this returns an array with all the phone_number objects from the requested Organizations
-def get_phone_numbers(phone_number)
-    orgs = Organization.all
-    organizations_phone_number_objects = []
-    orgs.each do |org|
-        organizations_phone_number_objects << org.phone_numbers
-    end
-    organizations_phone_number_objects.each do |phone|
-        phone.each do |number_object|
-            if number_object.number == phone_number
-                return number_object.organization_id
-            end
-        end
-    end
+# Returns Creates Active record Query to return organization based on an inputted phone number
+def get_provider_from_phone_number(input_number)
+    Organization.joins(:phone_numbers).where(:phone_numbers =>{number: input_number})
 end
 
-get_phone_numbers("(510) 251-2292")
+
 
