e2a3d061f992fe7b574b6e7e4513ca565f276dde
Reuben Brandt <Reubennbrandt@gmail.com>
2016-05-30 19:06:54 -0700

    Create early logic for complex searching

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,9 +1,15 @@
 class OrganizationsController < ActionController::API
     def index
-        if params.empty?
+        p params
+        test = params[:fee]
+        if  params[:languages]
             @organizations = Organization.all
+            p "#{params}"
+        elsif params[:genders]
+            @organizations = get_provider_from_gender(params[:genders])
         else
-             @organizations = Organization.where(organization_params)
+            puts " asdfasdff #{organization_params}"
+            @organizations = Organization.where(organization_params)
         end
         render json: @organizations
     end
@@ -18,7 +24,7 @@ class OrganizationsController < ActionController::API
         :phone_number,#created object, not searchable
         :email,
         :description,
-        :gender,
+        :genders,
         :age,
         :language,
         :service_hour,
@@ -31,5 +37,15 @@ def get_provider_from_phone_number(input_number)
     Organization.joins(:phone_numbers).where(:phone_numbers =>{number: input_number})
 end
 
+def get_provider_from_gender(input_gender)
+    p input_gender
+    Organization.joins(:genders).where(:genders =>{gender: input_gender})
+end
+
+
+
+# def get_provider_from_age(input_age)
+#     Organization.joins(:ages).where((:ages =>{:minimum_age <= input_age}) && (:ages =>{:maximum_age >= input_age}))
+# end
 
 
